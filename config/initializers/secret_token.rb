5610cfe00272058e04029f268aef3092166c51d5
rodrigotello <rodrigotello@outlook.com>
2014-01-13 12:21:08 -0600

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Superfantastic::Application.config.secret_token = 'ea991686d8a160de8baaf47b2a96fd44fe9ac2eaed7dca221efd3827a7d0ce9dc9d959cf6c03c528b5ab5151fa15000f1d393155fdfe0c30eee9e64587a6a4fe'
